4e69c224a4f56d50b0148d2bfd4b2d0581874557
bob <rlp1938@gmail.com>
2019-06-03 18:04:10 +0700

    Changed default font name and size.

--- fontfunc.sh
+++ fontfunc.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 #
-# fontfunc.sh - bash function to write font data to postsript file.
+# fontfunc.sh - bash function to write font data to postscript file.
 #
 # Copyright 2019 Robert L (Bob) Parker rlp1938@gmail.com
 #
 setfont()
 { # set up font specs in a postscript file
   local fn="$1"
-  echo '%% set up a font that may do the job' >> "$fn"
-  echo /Times-Roman findfont >> "$fn"
-  echo '%% Scale the font' >> "$fn"
-  echo 12 scalefont >> "$fn"
+  echo '% set up a font that may do the job' >> "$fn"
+  echo /Helvetica findfont >> "$fn"
+  echo '% Scale the font' >> "$fn"
+  echo 8 scalefont >> "$fn"
   echo setfont >> "$fn"
 }
